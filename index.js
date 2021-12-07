e73ea2e6a1b8b5545594e6686ebffa15a9919990
Mert OZSAHIN <94782649+mertozsahin@users.noreply.github.com>
2021-12-07 03:44:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
+
+var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + RandomNumber2 + ".png";
+var randomImageSource2 = "/images" + randomdiceimage2;
+document.querySelectorAll("img")[0]
